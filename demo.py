da000424fe1919106c777ca4c2c10c9df3c23cdd
sellputcapital <93781556+sellputcapital@users.noreply.github.com>
2021-11-05 08:22:50 -0700

    Added offset flag

--- demo.py
+++ demo.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
 
+offset = False
 
 # Generate and Sort Noisy Fake Data
 N = 1000
 x = np.sort(np.random.randn(N))
+if offset:
+    y = np.sort(np.random.randn(N)) - 0.25
 
 # Compute CDF and peaked CDF (pCDF)
 # The peaked CDF flips over at the median
@@ -19,6 +22,8 @@ fig, axes = plt.subplots(1,2, figsize=(12,5))
 axes[0].set_title('Traditional Density Estimation')
 axes[0].hist(x, bins=100, density=True, alpha=0.5, label='hist')
 axes[0].plot(x, st.norm.pdf(x), label='normal PDF')
+if offset:
+    axes[0].hist(y, bins=100, density=True, alpha=0.5, label='hist offset')
 axes[0].set_xlabel('Random Variable Values (Outcomes)')
 axes[0].set_ylabel('Probability Density')
 axes[0].legend()
@@ -26,6 +31,8 @@ axes[0].legend()
 axes[1].set_title('Peaked CDFs')
 axes[1].plot(x, pCDF, label='Measured pCDF')
 axes[1].plot(x, actualpCDF, label='normal pCDF')
+if offset:
+    axes[1].plot(y, pCDF, label='Measured offset pCDF')
 axes[1].set_xlabel('Random Variable Values (Outcomes)')
 axes[1].set_ylabel('Peaked CDF')
 axes[1].legend()
